14b036aeb7f5b93dae370200d9e23d067eab215a
Josh <joshrazonn@gmail.com>
2020-11-17 15:07:49 -0700

    added routing to index.ejs and gallery.ejs, added footer styles and resets to gallery.css

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const path = require('path');
 // Create express app
 const app = express();
+app.set('view engine', 'ejs');
 
 // app.use is for using middleware
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/gallery', (req, res) => res.render('./pages/gallery'));
+
+// index.ejs test
+app.get('/index2', (req, res) => res.render('./pages/index2'));
 
 app.use(function(req, res, next) {
   res.status(404);
