b184643236774df7fe48beddde8b7e15e56acabd
Josh <joshrazonn@gmail.com>
2020-11-17 16:36:44 -0700

    installed nodemon as dev dependency, finished initial gallery layout, downloaded gallery pics

---
+++ tours.js
@@ -0,0 +1,62 @@
+const tours = [
+  {
+    id:     1,
+    title:  'Hikers on W trek',
+    credit: 'Toomas Tartes',
+    url:    'https://unsplash.com/photos/Yizrl9N_eDA',
+    width:  500,
+    height: 300
+  },
+  {
+    id:     2,
+    title:  'Adventurous Mountain Hikes',
+    credit: 'Galen Crout',
+    url:    'https://unsplash.com/photos/fItRJ7AHak8',
+    width:  500,
+    height: 300,
+
+    imagePath: `<img src="./images/gallery/hike${this.id}.jpg" height="${this.height}" width="${this.width}" alt="${this.title}"></img>`
+  },
+  {
+    id:     3,
+    title:  'Untitled',
+    credit: 'Lucas Clara',
+    url:    'https://unsplash.com/photos/hvPB-UCAmmU',
+    width:  500,
+    height: 300,
+
+    imagePath: `<img src="./images/gallery/hike${this.id}.jpg" height="${this.height}" width="${this.width}" alt="${this.title}"></img>`
+  },
+  {
+    id:     4,
+    title:  'Untitled',
+    credit: 'Chiro',
+    url:    'https://unsplash.com/photos/xOigCUcFdA8',
+    width:  500,
+    height: 300,
+
+    imagePath: `<img src="./images/gallery/hike${this.id}.jpg" height="${this.height}" width="${this.width}" alt="${this.title}"></img>`
+  },
+  {
+    id:     5,
+    title:  'Above clouds in Triglav national park',
+    credit: 'Kristjan Kotar',
+    url:    'https://unsplash.com/photos/-h15p84GY5k',
+    width:  500,
+    height: 300,
+
+    imagePath: `<img src="./images/gallery/hike${this.id}.jpg" height="${this.height}" width="${this.width}" alt="${this.title}"></img>`
+  },
+  {
+    id:     6,
+    title:  'Sunrise hike EL Hoyo Volcano',
+    credit: 'Tim Foster',
+    url:    'https://unsplash.com/photos/lSegRSDBMLw',
+    width:  500,
+    height: 300,
+
+    imagePath: `<img src="./images/gallery/hike${this.id}.jpg" height="${this.height}" width="${this.width}" alt="${this.title}"></img>`
+  }
+];
+
+module.exports = tours;
